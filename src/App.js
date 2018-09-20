0f7ba9fa822fd2e477c730fac66acb7615710a63
candypoplatte <hwanght1@gmail.com>
2018-09-20 22:56:21 +0900

    Modify map을 활용해서 movies object array의 data의 갯수대로 Movie 컴포넌트 출력
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "God Father",
-  "Superman",
-  "Batman",
-  "Ironman"
-]
-
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg",
-  "https://i.ebayimg.com/00/s/NTAwWDMzOA==/z/7WQAAMXQ74JTWIJr/$_35.JPG?set_id=2",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8Q7FkVG4FCT-fsxJwTLYImZc4bUGrvMmDgKFpNBbhUm-oesU",
-  "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
+const movies = [
+  {
+    "title": "God Father",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg"
+  },
+  {
+    "title": "Superman",
+    "poster": "https://i.ebayimg.com/00/s/NTAwWDMzOA==/z/7WQAAMXQ74JTWIJr/$_35.JPG?set_id=2"
+  },
+  {
+    "title": "Batman",
+    "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8Q7FkVG4FCT-fsxJwTLYImZc4bUGrvMmDgKFpNBbhUm-oesU"
+  },
+  {
+    "title": "Ironman",
+    "poster": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+        {movies.map(movie => { 
+          return <Movie title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
   }
