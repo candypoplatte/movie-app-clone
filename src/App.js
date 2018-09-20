d647f793b7a2c6bcb99b1cf0d87f56a9d9f7b8e1
candypoplatte <hwanght1@gmail.com>
2018-09-21 00:59:05 +0900

    Modify 5초 Loading 후 영화목록 보여지도록 수정
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,48 +7,49 @@ class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillRecieveProps() -> shouldComponentUpdate() == true -> render() -> componentWillUpdate() -> componentDidUpdate()
 
-  state = {
-    movies: [
-      {
-        "title": "God Father",
-        "poster": "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg"
-      },
-      {
-        "title": "Superman",
-        "poster": "https://i.ebayimg.com/00/s/NTAwWDMzOA==/z/7WQAAMXQ74JTWIJr/$_35.JPG?set_id=2"
-      },
-      {
-        "title": "Batman",
-        "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8Q7FkVG4FCT-fsxJwTLYImZc4bUGrvMmDgKFpNBbhUm-oesU"
-      },
-      {
-        "title": "Ironman",
-        "poster": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
-      }
-    ]
-  }
+  state = {}
 
   componentDidMount() {
     setTimeout(() => { 
       this.setState({
         movies: [
           {
+            "title": "God Father",
+            "poster": "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg"
+          },
+          {
+            "title": "Superman",
+            "poster": "https://i.ebayimg.com/00/s/NTAwWDMzOA==/z/7WQAAMXQ74JTWIJr/$_35.JPG?set_id=2"
+          },
+          {
+            "title": "Batman",
+            "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8Q7FkVG4FCT-fsxJwTLYImZc4bUGrvMmDgKFpNBbhUm-oesU"
+          },
+          {
+            "title": "Ironman",
+            "poster": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
+          },
+          {
             "title": "hulk",
             "poster": "http://imgc.allpostersimages.com/img/posters/the-hulk_u-L-F3NEM90.jpg"
-          },
-          ...this.state.movies
+          }
         ]
       })
     }, 5000)
+  }
+
+  _renderMovies = () => { 
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
   }
 
   render() {
     console.log('render')
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => { 
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
