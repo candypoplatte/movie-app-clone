98736d0e0d7eb205d042bc4c15f95e6462347b69
candypoplatte <hwanght1@gmail.com>
2018-09-20 22:48:26 +0900

    Add 최상위 컴포넌트에서 movie title array, image link array 추가 후 props로 전달
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
+const movieTitles = [
   "God Father",
   "Superman",
   "Batman",
   "Ironman"
 ]
 
+const movieImages = [
+  "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/51KtPqh3nkL.jpg",
+  "https://www.posterduniya.com/wp-content/uploads/2016/10/The-Dark-Knight-Batman-Movie-Official-Poster2.jpg",
+  "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movies[0]} />
-        <Movie title={movies[1]} />
-        <Movie title={movies[2]} />
-        <Movie title={movies[3]} />
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
     );
   }
