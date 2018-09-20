c3208bf479b3a2d529f1fc3599b421da06137dd9
candypoplatte <hwanght1@gmail.com>
2018-09-21 00:44:47 +0900

    Modify movies object를 state로 이동
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    "title": "God Father",
-    "poster": "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg"
-  },
-  {
-    "title": "Superman",
-    "poster": "https://i.ebayimg.com/00/s/NTAwWDMzOA==/z/7WQAAMXQ74JTWIJr/$_35.JPG?set_id=2"
-  },
-  {
-    "title": "Batman",
-    "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8Q7FkVG4FCT-fsxJwTLYImZc4bUGrvMmDgKFpNBbhUm-oesU"
-  },
-  {
-    "title": "Ironman",
-    "poster": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
-  }
-]
-
 class App extends Component {
 
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillRecieveProps() -> shouldComponentUpdate() == true -> render() -> componentWillUpdate() -> componentDidUpdate()
 
   state = {
-    greeting: "hello"
+    movies: [
+      {
+        "title": "God Father",
+        "poster": "https://images-na.ssl-images-amazon.com/images/I/41a37t9BwTL.jpg"
+      },
+      {
+        "title": "Superman",
+        "poster": "https://i.ebayimg.com/00/s/NTAwWDMzOA==/z/7WQAAMXQ74JTWIJr/$_35.JPG?set_id=2"
+      },
+      {
+        "title": "Batman",
+        "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8Q7FkVG4FCT-fsxJwTLYImZc4bUGrvMmDgKFpNBbhUm-oesU"
+      },
+      {
+        "title": "Ironman",
+        "poster": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513"
+      }
+    ]
   }
 
   componentDidMount() {
@@ -42,7 +40,7 @@ class App extends Component {
     console.log('render')
     return (
       <div className="App">
-        {movies.map((movie, index) => { 
+        {this.state.movies.map((movie, index) => { 
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
